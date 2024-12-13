f6843c3e529271bfefe6b6db0e6c98339c49e9c8
ateeb <ateebnaveed1996@gmail.com>
2024-12-13 20:31:02 +0500

    sqlite database

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -171,17 +171,22 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'bedb',
+#         'USER': 'bedb',
+#         'PASSWORD': 'A053730730a',
+#         'HOST': 'bedb.cr2ywaqoihsx.us-east-1.rds.amazonaws.com',
+#         'PORT': '5432',
+#         }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'bedb',
-        'USER': 'bedb',
-        'PASSWORD': 'A053730730a',
-        'HOST': 'bedb.cr2ywaqoihsx.us-east-1.rds.amazonaws.com',
-        'PORT': '5432',
-        }
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
-
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
